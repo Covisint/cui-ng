deb5cb7b829bdcef764c2683b579bb4784481c53
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2017-05-12 11:20:26 +0530

    Initial changes of directive and corresponding showcase changes

---
+++ directives/cui-tree-alt/README.md
@@ -0,0 +1,58 @@
+# CUI-Tree-Alternative
+Version 1.0
+
+
+### Description
+Cui-Tree-alternative is a directive that generates a tree like markup structure with expandable and collapsable functionalities, based on an array of objects with 'children' .
+
+### Usage Example
+
+```html
+  <div class="cui-tree-alt" cui-tree-alt="app.tree" cui-tree-alt-leaf-display="object.text"></div>
+```
+
+* cui-tree-alt-leaf-display determines what to show in each leaf, and it accepts: properties from each object (use object.propertyName), strings ( wrap them in '' ) and filters ( ex: (object.name | cuiI18n) ). You can also concat display values ( ex : cui-tree-alt-leaf-display="object.text + ' ' + object.text2" )
+
+This directive expects Second and down the level objects with children to have one more boolean property "expanded" set intially to false.
+```javascript
+    app.tree=[
+        {
+            text:"I'm a parent node",
+            children: [{
+                text:"I'm a child with Children",
+                children:[{
+                text:"I am a second level child"
+                }],
+                expanded:false
+            },
+            {
+                text:"I'm a child without Children"
+            }]
+        },
+        {
+            text:"I'm a sibling of the parent"
+        }
+    ];
+```
+
+### Optional attributes
+
+Note:
+* by "nesting level" we're referring to the nesting of a certain "leaf" in a tree. So a top level parent has nesting level 0, a child of that parent has nesting level 1, its child has nesting level 2, and so on.
+* a "leaf" is an object in our tree. A "branch" is a conglomerate of a branch and it's children.
+
+* `cui-tree-alt-nest-0-class` (default: 'cui-tree-alt--nesting-0') - class that wraps the whole tree, starting at level 0 nesting.
+* `cui-tree-alt-nest-x-class` (default: 'cui-tree-alt--nested') - class that wraps each "branch" that has at least level 1 nesting.
+* `cui-tree-alt-leaf-wrapper`(default:'<div class="cui-tree-alt__leaf"></div>') - html wrapper for each leaf in the tree
+* `cui-tree-alt-last-leaf-class` (default: 'cui-tree-alt__leaf--last') - class to apply to a leaf it if its the last leaf on that branch
+* `cui-tree-alt-branch-wrapper` (default:'<div class="cui-tree-alt__branch"></div>') - html wrapper for each branch
+* `cui-tree-alt-last-branch-class` (default: 'cui-tree-alt__branch--last') - class to wrap the parent leaf and it's children if that leaf is the last leaf on that nesting level
+* `cui-tree-alt-nest-prefix` (default: 'cui-tree-alt--nesting-) - class prefix to apply to every nesting level after level 0. (would be cui-tree-alt--nesting-1, cui-tree-alt--nesting-2, and so on)
+* `cui-tree-alt-leaf-click-callback` - callback function to a click on a leaf. This function is called with 3 params - the object on the leaf that got clicked, the leaf html element that got clicked (so we can manipulate classes) and the JS click event, in that order.
+
+
+![cui-tree-alt-example](https://github.com/thirdwavellc/cui-ng/blob/master/directives/cui-tree-alt/cui-tree-alt.png?raw=true)
+
+## Change Log 5/17/2016
+
+* Now using scope.$eval to parse the display value. No breaking changes.
---
+++ directives/cui-tree-alt/cui-tree-alt.js
@@ -0,0 +1,111 @@
+const defaultsAlt = {
+    cuiTreeAltNest0Class : 'cui-tree-alt--nesting-0',
+    cuiTreeAltNestXClass : 'cui-tree-alt--nested',
+    cuiTreeAltLeafWrapper: '<div class="cui-tree-alt__leaf"></div>',
+    cuiTreeAltLastLeafClass : 'cui-tree-alt__leaf--last',
+    cuiTreeAltBranchWrapper: '<div class="cui-tree-alt__branch"></div>',
+    cuiTreeAltLastBranchClass : 'cui-tree-alt__branch--last',
+    cuiTreeAltNestPrefix : 'cui-tree-alt--nesting-'
+};
+
+const cuiTreeAltHelpers = {
+    getDisplayValue:(scope, opts, object) => {
+        const { cuiTreeAltLeafDisplay } = opts;
+        let propertiesToDisplay = cuiTreeAltLeafDisplay.split('+');
+
+        return scope.$eval(cuiTreeAltLeafDisplay, { object });
+    },
+    getClassListForNestingLevel: (opts,nesting) => {
+        const { cuiTreeAltNestPrefix, cuiTreeAltNest0Class, cuiTreeAltNestXClass } = opts;
+        let classList = [];
+        switch (nesting){
+            case 0:
+                classList.push( cuiTreeAltNest0Class || defaultsAlt.cuiTreeAltNest0Class );
+                break;
+            default:
+                classList.push((cuiTreeAltNestPrefix || defaultsAlt.cuiTreeAltNestPrefix) + nesting);
+                classList.push( cuiTreeAltNestXClass || defaultsAlt.cuiTreeAltNestXClass );
+        };
+        return classList;
+    },
+    getElements : (scope, opts, objects, leafClickCallback, toggleExpandCallback,nesting=0, parent) => {
+        const { getElements, getDisplayValue, getClassListForNestingLevel } = cuiTreeAltHelpers;
+        const { cuiTreeAltBranchWrapper, cuiTreeAltLeafWrapper, cuiTreeAltLastLeafClass, cuiTreeAltLastBranchClass } = opts;
+        let $node = $(`<div></div>`);
+        getClassListForNestingLevel(opts,nesting).forEach(className => $node[0].classList.add(className));
+        objects.forEach((object,i) => {
+            const $leafInner = $(`<span></span>`);
+            const $leafExpandIcon=$(`<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" class="cui-tree-icon" viewBox="0 0 216 146">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="node_modules/@covisint/cui-icons/dist/font-awesome/font-awesome-out.svg#plus27"></use>
+                </svg>`)
+            const $leafCollapseIcon=$(`<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" class="cui-tree-icon" viewBox="0 0 216 146">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="node_modules/@covisint/cui-icons/dist/font-awesome/font-awesome-out.svg#minus19"></use>
+                </svg>`)
+            const $leafInnerText = $(`<span>${ getDisplayValue(scope, opts, object) }</span>`);
+            // const $leafInnerExpandIcon = $(`<span>t</span>`) ;
+            if (!parent) {
+                if (!object.children) {
+                    $leafInner.append($leafExpandIcon);
+                    $leafExpandIcon[0].classList.add('cui-tree-hide-icon')
+                }else{
+                    if (object.expanded) {
+                        $leafInner.append($leafCollapseIcon);
+                    }else{
+                        $leafInner.append($leafExpandIcon);
+                    }
+                }
+                if(leafClickCallback) $leafInnerText[0].addEventListener("click",function(e){ leafClickCallback(object,this,e) },true);
+                if(toggleExpandCallback) $leafInner[0].children[0].addEventListener("click",function(e){ toggleExpandCallback(object,this,e) },true);
+            }
+            $leafInner.append($leafInnerText);
+            const $leafWrapper = $(cuiTreeAltLeafWrapper || defaultsAlt.cuiTreeAltLeafWrapper);
+            $leafWrapper.append($leafInner);
+            if(i === objects.length-1) $leafWrapper[0].classList.add(cuiTreeAltLastLeafClass || defaultsAlt.cuiTreeAltLastLeafClass); // add class to last leaf of each indent level.
+            if(object.children&&(parent||object.expanded===true)) { // if it has children creat a new branch for the leaf and it's children
+                const $branchWrapper = $(cuiTreeAltBranchWrapper || defaultsAlt.cuiTreeAltBranchWrapper).append($leafWrapper);
+                if(i === objects.length-1) $branchWrapper[0].classList.add(cuiTreeAltLastBranchClass || defaultsAlt.cuiTreeAltLastBranchClass);
+                $branchWrapper.append(getElements(scope, opts, object.children, leafClickCallback, toggleExpandCallback, nesting + 1)); // recursively gets the child nodes
+                $node.append($branchWrapper);
+            }
+            else {
+                $node.append($leafWrapper);
+            }
+        });
+        return $node;
+    }
+};
+
+const cuiTreeAlt = {
+    pre: (scope,elem,attrs) => {
+        let $tree;
+        const leafClickCallback = scope.$eval(attrs.cuiTreeAltLeafClickCallback);
+        const toggleExpandCallback = scope.$eval(attrs.cuiTreeAltToggleExpandCallback);
+        const renderTree = (tree) => {
+            if($tree) {
+                $tree.detach();
+                $tree.children().unbind();
+            }
+            $tree = cuiTreeAltHelpers.getElements(scope, attrs, tree, leafClickCallback,toggleExpandCallback,0,true);
+            elem.append($tree);
+        };
+
+        scope.$watch(()=>scope.$eval(attrs.cuiTreeAlt),(newTree)=>{
+            if(newTree) renderTree(newTree);
+        },true);
+
+        scope.$on('$destroy',()=>{
+            $tree.children().unbind();
+        });
+    }
+};
+
+angular.module('cui-ng')
+.directive('cuiTreeAlt',[()=>{
+    return {
+        restrict:'A',
+        scope: true,
+        compile: ()=>{
+            return cuiTreeAlt;
+        }
+    }
+}]);
